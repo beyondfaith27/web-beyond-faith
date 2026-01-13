0f873f3804bd09dd6113fa4928443be58234442f
Alfred Nelson <alfrednelson375@gmail.com>
2026-01-13 10:28:49 +0530

    fix: responsive issues for footer

--- app/Footer.js
+++ app/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full  grid grid-cols-3 items-center px-[var(--gutter-width)] py-6 border-t border-foreground/5">
+    <footer className="w-full grid grid-cols-2 md:grid-cols-3 gap-y-6 items-center px-[var(--gutter-width)] py-6 border-t border-foreground/5">
       <h2 className="text-lg font-dm inline-block">
         <Image
           src="/images/beyondFaith-logo-black.svg"
@@ -14,7 +14,7 @@ const Footer = () => {
           draggable="false"
         />
       </h2>
-      <div className="flex items-center gap-x-6 tablet:gap-x-12 justify-center">
+      <div className="flex items-center gap-x-6 tablet:gap-x-12 justify-end md:justify-center">
         <Link href="#">
           <Image
             src="/images/instagram.svg"
@@ -40,17 +40,15 @@ const Footer = () => {
           />
         </Link>
       </div>
-      <div className="flex flex-col items-end">
+      <div className="flex flex-col items-start md:items-end">
         <div className="flex flex-col items-center">
           <Link
             href={"/terms-and-conditions"}
-            className="text-foreground/40 hover:text-colored-background transition ease-in-out hidden tablet:flex text-sm"
+            className="text-foreground/40 hover:text-colored-background transition ease-in-out flex text-sm"
           >
             Terms and conditions
           </Link>
-          <span className="opacity-40 hidden tablet:flex text-sm">
-            © 2024 BeyondFaith
-          </span>
+          <span className="opacity-40 flex text-sm">© 2024 BeyondFaith</span>
         </div>
       </div>
     </footer>
