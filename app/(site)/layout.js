df23228da7e7600d8076d70b3cad718f9abb6522
Alfred Nelson <alfrednelson375@gmail.com>
2026-01-26 19:26:59 +0530

    fix: useSearchParams causing component suspension

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -4,15 +4,24 @@ import Retractable from "@/components/Retractable";
 import { ContactFormProvider } from "@/lib/ContactFormContext";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Suspense } from "react";
+
+function Fallback() {
+    return <section className="w-full h-full flex justify-center items-center">
+        Something went wrong
+    </section>
+}
 
 export default function SiteLayout({ children }) {
     return (
-        <ContactFormProvider>
-            <Retractable>
-                <Header />
-            </Retractable>
-            {children}
-            <Footer />
-        </ContactFormProvider>
+        <Suspense fallback={<Fallback />}>
+            <ContactFormProvider>
+                <Retractable>
+                    <Header />
+                </Retractable>
+                {children}
+                <Footer />
+            </ContactFormProvider>
+        </Suspense>
     );
 }
